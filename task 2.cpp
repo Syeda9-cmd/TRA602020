2952def5b5ba72ac2719bf2f5adac75370d8a8a6
Syeda9-cmd <Syedatawasal9@gmail.com>
2026-08-10 09:44:15 +0500

    Update task 2.cpp

--- task 2.cpp
+++ task 2.cpp
@@ -1,32 +1,13 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-struct Data {
-    string name;    
-    string color;   
-};
-struct Car {
-    string model;      
-    float  topSpeed;   
-    int    gears;
-    Data   more;       
-};
-int main() {
-    Car car;
-    cout << "Enter car name   : "; cin >> car.more.name;
-    cout << "Enter car color  : "; cin >> car.more.color;
-    cout << "Enter model      : "; cin >> car.model;
-    cout << "Enter top speed  : "; cin >> car.topSpeed;
-    cout << "Enter no. of gears: "; cin >> car.gears;
-    if (car.topSpeed > 200 && car.gears > 4) {
-        cout << "Car Details";
-        cout << "Name     : " << car.more.name  << endl;
-        cout << "Color    : " << car.more.color << endl;
-        cout << "Model    : " << car.model      << endl;
-        cout << "Top Speed: " << car.topSpeed   << endl;
-        cout << "Gears    : " << car.gears      << endl;
-        cout << "Status   : Your car IS suitable for the race!"<<endl;
-    } else {
-        cout <<"Your car is NOT suitable for the race."<<endl;
-    }
+int main()
+{
+int arr[6]={10,20,30,40,50,60};
+  int *ptr=arr;
+  for(int i=0;i<6;i++)
+  {
+      cout<< *ptr<<endl;
+      ptr++;
+  }
     return 0;
 }
