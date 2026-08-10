e6d8d28aa65f360115a518fbe40887f66cfba6c3
Syeda9-cmd <Syedatawasal9@gmail.com>
2026-08-10 09:58:26 +0500

    Create file 8.cpp

---
+++ file 8.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+struct Date {
+    int month;
+    int day;
+    int year;
+};
+int main() {
+    Date d;
+    char slash;
+    cout << "Enter date (MM/DD/YYYY): ";
+    cin >> d.month >> slash >> d.day >> slash >> d.year;
+    cout << "Date: " << d.month << "/"
+         << d.day << "/" << d.year << endl;
+    return 0;
+}
