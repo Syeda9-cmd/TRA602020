8406285a6409a541dd6c4125ae0dd99abd3796f6
Syeda9-cmd <Syedatawasal9@gmail.com>
2026-08-10 09:58:38 +0500

    Update task 4.cpp

--- task 4.cpp
+++ task 4.cpp
@@ -1,20 +1,19 @@
 #include<iostream>
 using namespace std;
-int main()
-{
-int a;
-int b;
-int *ptrA;
-int *ptrB;
-ptrA=&a;
-ptrB=&b;
-cout<<"enter a value of a:";
-cin>>a;
-cout<<"enter a value of b:";
-cin>>b;
-cout<<"assigning value of a :"<<*ptrA<<endl;
-cout<<"assigning value of b:"<<*ptrB<<endl;
-cout<<"adress stored in ptrA:"<<ptrA<<endl;
-cout<<"adress stored in ptrB:"<<ptrB<<endl;
+
+struct Phone {
+    int areaCode;
+    int exchange;
+    int number;
+};
+int main() {
+    Phone p1 = {212, 767, 8900};
+    Phone p2;
+    cout << "Enter your area code, exchange, and number: ";
+    cin >> p2.areaCode >> p2.exchange >> p2.number;
+    cout << "My number is (" << p1.areaCode << ") "
+         << p1.exchange << "-" << p1.number << endl;
+    cout << "Your number is (" << p2.areaCode << ") "
+         << p2.exchange << "-" << p2.number << endl;
     return 0;
 }
