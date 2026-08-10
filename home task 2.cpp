4307e50930c8a22ba15c735450d54d1aad675d0a
Syeda9-cmd <Syedatawasal9@gmail.com>
2026-08-10 09:43:38 +0500

    Update home task 2.cpp

--- home task 2.cpp
+++ home task 2.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 using namespace std;
-struct Parameters {
-    double length;
-    double width;
-};
-struct Result {
-    double area;
-    double perimeter;
-};
-struct Rectangle {
-    Parameters info;    
-    Result     result;   
-};
 int main() {
-    Rectangle rect;
-    cout << "Enter length: "; cin >> rect.info.length;
-    cout << "Enter width : "; cin >> rect.info.width;
-    rect.result.area      = rect.info.length * rect.info.width;
-    rect.result.perimeter = 2 * (rect.info.length + rect.info.width);
-    cout << "\n--- Rectangle Results ---\n";
-    cout << "Length    : " << rect.info.length    << endl;
-    cout << "Width     : " << rect.info.width     << endl;
-    cout << "Area      : " << rect.result.area      << endl;
-    cout << "Perimeter : " << rect.result.perimeter << endl;
+    char str[100];
+    cout << "Enter a string: ";
+    cin >> str;
+    char *ptr = str;
+    int length = 0;
+    while (*ptr != '\0') {
+        length++;
+        ptr++;
+    }
+    ptr--;
+    cout << "Length of string: " << length << endl;
+    cout << " Reverse string:";
+    for (int i = 0; i < length; i++) {
+        cout << *ptr;
+        ptr--;   
+    }
+    cout << endl;
     return 0;
 }
